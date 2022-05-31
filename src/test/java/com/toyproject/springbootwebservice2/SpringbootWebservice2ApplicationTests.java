ebca966c340fc7a3ce9d0945ca8e8ec9144fbc02
aken <aken@kakao.com>
2022-05-31 18:23:39 +0900

    Initial commit

---
+++ src/test/java/com/toyproject/springbootwebservice2/SpringbootWebservice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.springbootwebservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebservice2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
